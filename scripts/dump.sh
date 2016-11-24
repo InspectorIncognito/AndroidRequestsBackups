1b5c053b267168c0e2f8a9308a404b5526e5e463
matias pavez <matias.pavez.b@gmail.com>
2016-11-24 13:23:33 -0300

    added check for parameter as positive integer

--- scripts/dump.sh
+++ scripts/dump.sh
@@ -114,6 +114,11 @@ if [ "$BKP_TYPE" = "partial" ] && [ -z "$PARTIAL_BKP_TIME" ] ; then
 	echo "e.g: '5' for a 5 minutes lookup"
 	exit 1
 fi
+number_regex='^[1-9][0-9]*$'
+if ! [[ $PARTIAL_BKP_TIME =~ $number_regex ]] ; then
+   echo "PARTIAL_BKP_TIME must be a positive integer. given: $PARTIAL_BKP_TIME"
+   exit 1
+fi
 echo "  > PARTIAL_BKP_TIME: $PARTIAL_BKP_TIME"
 
 
