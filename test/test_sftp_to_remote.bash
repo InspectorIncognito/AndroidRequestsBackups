ee621aa161fee135e8ceddf8ff94d9e78a666427
matias pavez <matias.pavez.b@gmail.com>
2016-12-01 11:07:08 -0300

    remote tests ready

--- test/test_sftp_to_remote.bash
+++ test/test_sftp_to_remote.bash
@@ -6,17 +6,18 @@
 # 
 # To do this, the following procedure is executed:
 # - checks the required ssh key is present
-# - attempts to send a empty file through sftp to the remote
+# - attempts to send a file through sftp to the remote
 #
 
-REMOTE_USER=
-REMOTE_HOST=
-SECRET_KEY=
+REMOTE_USER="$1"
+REMOTE_HOST="$2"
+SECRET_KEY="$3"
+BACKUP_FOLDER="$4"
 
-REMOTE_USER=mpavez
-REMOTE_HOST=localhost
-SECRET_KEY=/home/mpavez/.ssh/id_rsa
-
+REMOTE_USER="mpavez"
+REMOTE_HOST="localhost"
+SECRET_KEY="/home/mpavez/.ssh/id_rsa"
+BACKUP_FOLDER="/home/mpavez/bkps/test"
 
 # settings
 echo "################################################################################"
@@ -26,15 +27,16 @@ echo "---------"
 echo "using REMOTE_USER: $REMOTE_USER"
 echo "using REMOTE_HOST: $REMOTE_HOST"
 echo "using SECRET_KEY: $SECRET_KEY"
+echo "using BACKUP_FOLDER: $BACKUP_FOLDER"
 echo "---------"
 REMOTE_USERHOST="$REMOTE_USER"@"$REMOTE_HOST"
 
 # checks
-if [ ! -e "$SECRET_KEY" ]; then
-	(>&2 echo " - (FAIL) ssh private key file not found: $SECRET_KEY.")
+if [ ! -r "$SECRET_KEY" ]; then
+	(>&2 echo " - (FAIL) ssh private key file not found or not readable: '$SECRET_KEY'.")
 	exit 1 
 fi
-echo " - (OK): found ssh private key file: $SECRET_KEY."
+echo " - (OK) found ssh private key file: '$SECRET_KEY'."
 
 # file to send
 TEST_SFTP_COMMANDS="/tmp/test_android_requests_backups_sftp"
@@ -42,23 +44,25 @@ TEST_SFTP_COMMANDS="/tmp/test_android_requests_backups_sftp"
 # create dummy sftp commands
 rm -f "$TEST_SFTP_COMMANDS"
 if [ -e "$TEST_SFTP_COMMANDS" ]; then
-	(>&2 echo " - (FAIL) Failed to delete previous tmp file: $TEST_SFTP_COMMANDS.")
+	(>&2 echo " - (FAIL) Failed to delete previous tmp file: '$TEST_SFTP_COMMANDS'.")
 	exit 1 
 fi
-echo "cd /home/$REMOTE_USER" > "$SFTP_COMMANDS"
+echo "cd $BACKUP_FOLDER" > "$TEST_SFTP_COMMANDS"
 echo "put $TEST_SFTP_COMMANDS" >> "$TEST_SFTP_COMMANDS"
-echo " - (OK) sent file by sftp."
+echo " - (OK) generated sftp batch file on '$TEST_SFTP_COMMANDS'."
 
 # send
 sftp -p -i "$SECRET_KEY" -b "$TEST_SFTP_COMMANDS" "$REMOTE_USERHOST"
 if [ $? -ne 0 ]; then
-	(>&2 echo " - (FAIL) there was a problem while sending the sftp file $TEST_SFTP_COMMANDS.")
+	(>&2 echo " - (FAIL) there was a problem while sending the sftp file '$TEST_SFTP_COMMANDS'.")
 	exit 1
 fi
 echo " - (OK) sent file by sftp."
 
 # end
-echo " - (WIN) . The SFTP test to a remote host passed successfully."
+echo " - (WIN) The SFTP test to a remote host passed successfully."
+echo "################################################################################"
+echo ""
 exit 0
 #############################################################################################################
 
--- test/test_ssh_to_remote.bash
+++ test/test_ssh_to_remote.bash
@@ -8,12 +8,13 @@
 # - checks the required ssh key is present
 # - attempts to create an ssh connection to the remote server
 # - attempts to run an script file through ssh, on the remote server
+#   the script will attempt to create a test folder and a test file
 #
 
-REMOTE_USER=
-REMOTE_HOST=
-SECRET_KEY=
-BACKUP_FOLDER=
+REMOTE_USER="$1"
+REMOTE_HOST="$2"
+SECRET_KEY="$3"
+BACKUP_FOLDER="$4"
 
 REMOTE_USER="mpavez"
 REMOTE_HOST="localhost"
@@ -35,35 +36,43 @@ REMOTE_USERHOST="$REMOTE_USER"@"$REMOTE_HOST"
 
 
 # checks
-if [ ! -e "$SECRET_KEY" ]; then
-	(>&2 echo " - (FAIL) ssh private key file not found: $SECRET_KEY.")
+if [ ! -r "$SECRET_KEY" ]; then
+	(>&2 echo " - (FAIL) ssh private key file not found or not readable: '$SECRET_KEY'.")
 	exit 1 
 fi
-echo " - (OK): found ssh private key file: $SECRET_KEY."
+echo " - (OK) found ssh private key file: '$SECRET_KEY'."
 
 
 # ssh connection
 ssh -i "$SECRET_KEY" "$REMOTE_USERHOST" -q exit
 if [ $? -ne 0 ]; then
-	(>&2 echo " - (FAIL) there was a problem while trying to stablish an ssh connection to the remote server $REMOTE_USERHOST.")
+	(>&2 echo " - (FAIL) there was a problem while trying to stablish an ssh connection to the remote server '$REMOTE_USERHOST'.")
 	exit 1
 fi
 echo " - (OK) ssh connection works."
 
+
 # ssh script
 TEST_SCRIPT="/tmp/test_android_requests_backups_ssh.bash"
 rm -f "$TEST_SCRIPT"
-echo "mkdir -p $BACKUP_FOLDER" >> "$TEST_SCRIPT"
-echo "if [ ! -e \"$BACKUP_FOLDER\" ]; then exit 1; else exit 0; fi"
-ssh -i "$PRIVATE_KEY" "$REMOTE_USERHOST" "bash -s" -- < "$TEST_SCRIPT"
+echo "mkdir -p \"$BACKUP_FOLDER\""                             >> "$TEST_SCRIPT"
+echo "if [ ! -d \"$BACKUP_FOLDER\" ]; then exit 1; fi"         >> "$TEST_SCRIPT"
+echo "rm -f \"$BACKUP_FOLDER\"/test_ssh_file"                  >> "$TEST_SCRIPT"
+echo "if [ -e \"$BACKUP_FOLDER\"/test_ssh_file ]; then echo \"Failed to remove the file $BACKUP_FOLDER/test_ssh_file\"; exit 1; fi" >> "$TEST_SCRIPT"
+echo "touch \"$BACKUP_FOLDER/test_ssh_file\""                  >> "$TEST_SCRIPT"
+echo "if [ ! -w \"$BACKUP_FOLDER\" ]; then exit 1; exit 0; fi" >> "$TEST_SCRIPT"
+echo "exit 0"                                                  >> "$TEST_SCRIPT"
+ssh -i "$SECRET_KEY" "$REMOTE_USERHOST" "bash -s" -- < "$TEST_SCRIPT"
 if [ $? -ne 0 ]; then
-	(>&2 echo " - (FAIL): SSH script exited  with non zero status. Maybe, the $REMOTE_USER do not have the proper permissions to create the folder: $BACKUP_FOLDER")
+	(>&2 echo " - (FAIL): SSH script exited  with non zero status. Maybe, the '$REMOTE_USER' user does not has the proper permissions to create the folder: '$BACKUP_FOLDER'.")
 	exit 1
 fi
 echo " - (OK) ssh script worked ok. successfully connected and ran script on remote."
 
 # end
-echo " - (WIN) . The SFTP test to a remote host passed successfully."
+echo " - (WIN) The SSH test to a remote host passed successfully."
+echo "################################################################################"
+echo ""
 exit 0
 #############################################################################################################
 
