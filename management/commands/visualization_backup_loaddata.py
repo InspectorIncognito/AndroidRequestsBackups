aa2ca52be6cb0b6de2f9237cdafb696f3397fa7b
matias pavez <matias.pavez.b@gmail.com>
2016-12-19 16:55:00 -0300

    attempt to mark partial models for transform

--- management/commands/visualization_backup_loaddata.py
+++ management/commands/visualization_backup_loaddata.py
@@ -6,6 +6,13 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
 
+        to_transform = [
+            'dump_Report.json',
+            'dump_EventForBusStop.json',
+            'dump_EventForBusv2.json',
+            'dump_Busv2.json'
+        ]
+
         # we must load the files in order!
         list_json = [
             'dump_Report.json',
@@ -18,15 +25,19 @@ class Command(BaseCommand):
         ]
 
         for filename in list_json:
-            self.from_JSON(self.save_to_database_cb, filename)
+            mark_for_transform = True if filename in to_transform else False
+            self.from_JSON(self.save_to_database_cb, filename, mark_for_transform)
             # self.from_JSON(self.print_cb, filename)
 
 
-    def from_JSON(self, callback, filename):
+    def from_JSON(self, callback, filename, mark_for_transform):
         print("loading data from: " + filename)
         cnt = 0
         with open(filename, 'r') as file:
             for deserialized_object in serializers.deserialize("json", file, ignorenonexistent=True):
+                if mark_for_transform:
+                    deserialized_object.object.transformed = False
+
                 callback(deserialized_object)
                 cnt += 1
         print(" . . . loaded " + str(cnt) + " rows.")
